b8784b064e037a4b2080487cb4009cb03bdfc534
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-07 11:06:02 +0100

    connects to pokemon api

--- poke-sup/src/pages/Pokemon/index.jsx
+++ poke-sup/src/pages/Pokemon/index.jsx
@@ -1,6 +1,19 @@
-// import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Pokemon = () => {
+  const [ pokemon, setPokemon ] = useState([]);
+
+  useEffect(() => {
+    async function data(id){
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const response = await data.json();
+      setPokemon(response);
+      console.log(response);
+    } 
+
+    data(1);
+  },[])
+
   return (
     <div>Pokemon</div>
   )
