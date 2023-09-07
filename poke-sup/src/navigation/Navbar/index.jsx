19d9609aa9445ea84ba59b8bf55bc7d26fa03f08
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-07 13:15:46 +0100

    fixes navbar css

--- poke-sup/src/navigation/Navbar/index.jsx
+++ poke-sup/src/navigation/Navbar/index.jsx
@@ -3,12 +3,14 @@ import { NavLink, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+    <>
     <nav>
         <NavLink to='/'>Home</NavLink>
-        <NavLink to='/battle'>Battle</NavLink>
+        <NavLink to='/battle'>Teams</NavLink>
         <NavLink to='/pokemon'>Pokemon</NavLink>
-        <Outlet />
     </nav>
+    <Outlet />
+    </>
   )
 }
 
